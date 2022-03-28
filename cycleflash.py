380c59b900f83e19f544e212f370278c23b3e32e
unknown <Simon Wang>
2022-03-28 21:32:16 +0800

    Commit Tv0.1

--- cycleflash.py
+++ cycleflash.py
@@ -5,6 +5,7 @@ import ipaddress
 import argparse
 import json
 import time
+import datetime
 import re
 from textwrap import dedent
 from lib.utils import excute
@@ -49,6 +50,7 @@ class FirmwareFlash:
             put_log(self.flashlog, out)
             logger.info(' - Execute Result:')
             logger.info('   ' + str(res))
+            return res
         except RuntimeError as e:
             logger.error(e)
 
@@ -118,21 +120,28 @@ class CycleFlash(FirmwareFlash):
             pass
 
     def wait_active(self):
-        wait_os_up = 1000
+        wait_os_up = 1200
         wait_bmc_up = 80
         flag = False
         if self.flashtype in ['CPLD', 'BIOS']:
+            logger.info('Waiting {0} Seconds for Host Up'.format(str(wait_os_up)))
             time.sleep(wait_os_up)
             for i in range(10):
                 if os.system('ping -c 1 {0} > /dev/null 2>&1'.format(HOSTIP)):
                     flag = True
                     break
+                logger.info('Retry Connect Host {0} times'.format(str(i)))
                 time.sleep(5)
             # raise RuntimeError('Host is Hang, Can\'t Ping Host:' + HOSTIP)
         else:
+            logger.info('Waiting {0} Seconds for BMC Up'.format(str(wait_os_up)))
             time.sleep(wait_bmc_up)
             for i in range(10):
-                pass
+                # if os.system('ping -c 1 {0} > /dev/null 2>&1'.format(HOSTIP)):
+                #     flag = True
+                #     break
+                logger.info('Retry Connect BMC {0} times'.format(str(i)))
+                time.sleep(5)
             raise RuntimeError('BMC is dead')
 
     def check(self):
@@ -140,10 +149,15 @@ class CycleFlash(FirmwareFlash):
 
     def compare_ver(self, criteria_ver, current_ver):
         res = ''
+        logger.info('Starting Compare Firmware Version')
+        logger.info(' - Current: ' + current_ver)
+        logger.info(' - Criteria: ' + criteria_ver)
         if criteria_ver == current_ver:
             res = 'PASS'
+            logger.info('Compare PASS')
         else:
             res = 'FAIL'
+            logger.error('Compare FAIL')
         return res
 
     def summary(self):
@@ -177,16 +191,14 @@ class CycleFlash(FirmwareFlash):
             logger.info(self.template.format(self.split_line, str(count), self.flashtype,
                                              op, pre_ver, flashversion, flashimage))
             put_log(self.flashlog, 'N0. {0} Flash {1} firmware {2}\n'.format(str(count), self.flashtype, op))
-            self.flash(flashimage)
+            flash_res = self.flash(flashimage)
             self.activemode()
             self.wait_active()
-            put_log(self.summary_log, 'NO. {0} Flash {1} firmware {2}\n'.format(str(count), self.flashtype, op))
             cur_ver = self.getver()
             self.res['Current Version'] = cur_ver
             compare_res = self.compare_ver(flashversion, cur_ver)
             self.res['Result'] = compare_res
-            logger.info(self.res)
-            put_log(self.summary_log, str(self.res))
+            put_log(self.summary_log, str(datetime.datetime.now) + str(self.res))
             self.res_list.append(self.res)
         self.summary()
 
@@ -209,12 +221,12 @@ if __name__ == '__main__':
     loops = args['loops']
     HOSTIP = '192.168.0.35'
     IPMI = 'ipmitool -I lanplus -H {ip} -U {username} -P {password}'.format(**args)
-    # if os.system(IPMI + ' raw 6 1 > /dev/null 2>&1'):
-    #     raise RuntimeError('Check BMC Overlan Fail, Please Check BMC Info.')
+    if os.system(IPMI + ' raw 6 1 > /dev/null 2>&1'):
+        raise RuntimeError('Check BMC Overlan Fail, Please Check BMC Info.')
     config = ConfigParser(os.path.join(HOMEDIR, 'config.yml'))
     uTool = config.get_utool()
-    # if not os.path.exists(uTool):
-        # raise RuntimeError('No Such Utool:' + uTool)
+    if not os.path.exists(uTool):
+        raise RuntimeError('No Such Utool:' + uTool)
     # flash_cfg = config.get_flashcfg(flashtype)
     # up_images = flash_cfg['upgrade']
     # down_images = flash_cfg['download']
